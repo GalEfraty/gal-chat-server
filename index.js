f80ca89d3a88262df4cbf1058654faaeb3dd77bd
Gal Efraty <gal.efraty@gmail.com>
2020-02-10 16:28:37 +0200

    setup basic architecture

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors")
+
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 
@@ -7,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors())
 require("./routes/chatRoutes")(app);
 
 app.listen(PORT, () => {
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,7 +1,14 @@
 const requireLogin = require("../middlewares/requireLogin");
+
 const keys = require("../config/keys")
 
 module.exports = app => {
+    // app.get("/api/hello", (req, res) => {
+
+    //     console.log(`hello ${keys.someConfigVar}`)
+    //     res.status(200).send({message: `hello back from server ${keys.someConfigVar}`})
+    // })
+
     app.get("/api/hello", (req, res) => {
 
         console.log(`hello ${keys.someConfigVar}`)
