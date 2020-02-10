d7b1ed77ce367340f6e0ffaea0cf79aafae46d88
Gal Efraty <gal.efraty@gmail.com>
2020-02-10 15:02:55 +0200

    init commit

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,8 +1,10 @@
 const requireLogin = require("../middlewares/requireLogin");
+const keys = require("../config/keys")
 
 module.exports = app => {
     app.get("/api/hello", (req, res) => {
-        console.log("hello")
-        res.status(200).send({message: "hello back from server"})
+
+        console.log(`hello ${keys.someConfigVar}`)
+        res.status(200).send({message: `hello back from server ${keys.someConfigVar}`})
     })
 }
